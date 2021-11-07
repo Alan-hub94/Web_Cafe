42f3c20279a364bfb449a4059482177d94dcb870
Alan-hub94 <cereal9494@gmai.com>
2021-11-06 22:51:29 -0300

    cafe-V2

--- My_cafe/urls.py
+++ My_cafe/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from django.urls.conf import include
+from django.conf import settings
 
 urlpatterns = [ 
+    #PATH Admin
     path('admin/', admin.site.urls),
+    #PATH core
     path('', include('core.urls')),
+    #PATH services
+    path('services/', include('services.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, documen_root=settings.MEDIA_ROOT)
+
+    
